63573d8ed1ece54b90ef1965f3f6d39e6953f36f
Matheus Andrade <matheus.andrade@gesplan.com.br>
2023-11-06 18:44:31 -0300

    final

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next'
+
+import { Urbanist } from 'next/font/google'
+
+import Footer from '@/components/footer'
+import NavBar from '@/components/navbar'
+import ModalProvider from '@/providers/modal-provider'
+import ToastProvider from '@/providers/toast-provider'
+
+import './globals.css'
+
+const font = Urbanist({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Store',
+  description: 'Store',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ModalProvider />
+        <ToastProvider />
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
