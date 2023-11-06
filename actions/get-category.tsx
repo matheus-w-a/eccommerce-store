4b8f7cc5fa98cea5814321b7afe91473cbb8e0c2
Matheus Andrade <matheus.andrade@gesplan.com.br>
2023-11-06 19:21:54 -0300

    changed to fetch

--- actions/get-category.tsx
+++ actions/get-category.tsx
@@ -4,9 +4,9 @@ import axios from 'axios'
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`
 
 const getCategory = async (id: string): Promise<Category> => {
-    const res = await axios.get(`${URL}/${id}`)
+    const res = await fetch(`${URL}/${id}`)
 
-    return res.data
+    return res.json();
 }
 
 export default getCategory;
