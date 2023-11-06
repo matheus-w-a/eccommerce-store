bd61f59dfa1b73e1d39793c6d2cff0b68bfa241b
Matheus Andrade <matheus.andrade@gesplan.com.br>
2023-11-06 19:24:50 -0300

    changed all to fetch

--- actions/get-colors.tsx
+++ actions/get-colors.tsx
@@ -1,12 +1,12 @@
 import { Color } from '@/types'
 import axios from 'axios'
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/colors`
 
 const getColors = async (): Promise<Color[]> => {
-    const res = await axios.get(URL)
+    const res = await fetch(URL)
 
-    return res.data
+    return res.json()
 }
 
 export default getColors;
--- actions/get-sizes.tsx
+++ actions/get-sizes.tsx
@@ -1,12 +1,12 @@
 import { Size } from '@/types'
 import axios from 'axios'
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/sizes`
 
 const getSizes = async (): Promise<Size[]> => {
-    const res = await axios.get(URL)
+    const res = await fetch(URL)
 
-    return res.data
+    return res.json()
 }
 
 export default getSizes;
